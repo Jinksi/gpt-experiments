25235fb7a3757f0968745920ad0e7f85abc9fed5
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-03-10 16:48:04 +1000

    Add all WCPay docs to vector store as text

--- scripts/fetch-docs.ts
+++ scripts/fetch-docs.ts
@@ -6,67 +6,32 @@ import { CharacterTextSplitter } from 'langchain/text_splitter'
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import Turndown from 'turndown'
 
+import wcpayDocs from '../data/wcpay-docs.json' assert { type: 'json' }
+
 // Load .env.local into process.env
 dotenv.config({ path: '.env.local' })
 
-const urls = [
-  'https://woocommerce.com/document/woocommerce-payments/',
-  `https://woocommerce.com/document/woocommerce-payments/compatibility/countries/`,
-  'https://woocommerce.com/document/woocommerce-payments/deposits/',
-  'https://woocommerce.com/document/woocommerce-payments/deposits/deposit-schedule/',
-  `https://woocommerce.com/document/woocommerce-payments/deposits/instant-deposits/`,
-  `https://woocommerce.com/document/woocommerce-payments/deposits/scheduled-deposit-vs-instant-deposit/`,
-  `https://woocommerce.com/document/woocommerce-payments/deposits/deposit-currencies/`,
-  `https://woocommerce.com/document/woocommerce-payments/deposits/change-deposit-account-info/`,
-  `https://woocommerce.com/document/woocommerce-payments/deposits/change-deposit-schedule/`,
-  `https://woocommerce.com/document/woocommerce-payments/deposits/why-deposits-suspended/`,
-  `https://woocommerce.com/document/woocommerce-payments/deposits/deposit-schedule/`,
-  `https://woocommerce.com/document/woocommerce-payments/deposits/deposits-and-payouts/`,
-  `https://woocommerce.com/document/woocommerce-payments/testing-and-troubleshooting/`,
-  `https://woocommerce.com/document/woocommerce-payments/taxes/`,
-  `https://woocommerce.com/document/woocommerce-payments/built-in-subscriptions/`,
-  `https://woocommerce.com/document/woocommerce-payments/built-in-subscriptions/subscriptions-basics/`,
-  `https://woocommerce.com/document/woocommerce-payments/stripe-capital/`,
-  `https://woocommerce.com/document/woocommerce-payments/stripe-capital/overview/`,
-  `https://woocommerce.com/document/woocommerce-payments/stripe-capital/faq/`,
-  `https://woocommerce.com/document/woocommerce-payments/payment-methods/`,
-  `https://woocommerce.com/document/woocommerce-payments/payment-methods/apple-pay/`,
-  `https://woocommerce.com/document/woocommerce-payments/payment-methods/google-pay/`,
-  `https://woocommerce.com/document/woocommerce-payments/payment-methods/link-by-stripe/`,
-  `https://woocommerce.com/document/woopay-merchant-documentation/`,
-  `https://woocommerce.com/document/woocommerce-payments/our-policies/`,
-  `https://woocommerce.com/document/woocommerce-payments/our-policies/reserves/`,
-  `https://woocommerce.com/document/woocommerce-payments/our-policies/pci-compliance/`,
-  `https://woocommerce.com/document/woocommerce-payments/our-policies/woocommerce-payments-know-your-customer/`,
-  `https://woocommerce.com/document/woocommerce-payments/our-policies/connection/`,
-  `https://woocommerce.com/document/woocommerce-payments/our-policies/restricted-products/`,
-  `https://woocommerce.com/document/woocommerce-payments/our-policies/can-i-sell-alcohol-with-woocommerce-payments/`,
-  `https://woocommerce.com/document/woocommerce-payments/our-policies/cbd/`,
-  `https://woocommerce.com/document/woocommerce-payments/in-person-payments/`,
-  `https://woocommerce.com/document/woocommerce-payments/fraud-and-disputes/`,
-  `https://woocommerce.com/document/woocommerce-payments/fees-and-debits/`,
-  `https://woocommerce.com/document/woocommerce-payments/fees-and-debits/fees/`,
-  `https://woocommerce.com/document/woocommerce-payments/fees-and-debits/account-showing-negative-balance/`,
-  `https://woocommerce.com/document/woocommerce-payments/customization-and-translation/`,
-  `https://woocommerce.com/document/payments/faq/scheduled-deposit-vs-instant-deposit/`,
-  `https://woocommerce.com/document/woocommerce-payments/currencies/`,
-  `https://woocommerce.com/document/woocommerce-payments/account-management/`,
-  `https://woocommerce.com/document/payments/settings-guide/`,
-  `https://woocommerce.com/document/payments/managing-money/`,
-]
-
-const uniqueUrls = urls.filter((url, index) => urls.indexOf(url) === index)
+type Doc = {
+  url: string
+  title: string
+}
+const docs: Doc[] = wcpayDocs
 
 async function run() {
   const posts = []
-  const metadatas = []
-  const turndownService = new Turndown({
-    hr: '',
-    codeBlockStyle: 'fenced',
-    headingStyle: 'atx',
-  })
+  const metadatas: Doc[] = []
+  // const turndownService = new Turndown({
+  //   hr: '',
+  //   codeBlockStyle: 'fenced',
+  //   headingStyle: 'atx',
+  // })
 
-  for (const url of uniqueUrls) {
+  for (const { url, title } of docs) {
+    if (metadatas.find((metadata) => metadata.url === url)) {
+      console.log(`${url} already fetched`)
+      continue
+    }
+
     const response = await fetch(url)
     const html = await response.text()
     const $ = cheerio.load(html)
@@ -81,21 +46,22 @@ async function run() {
         $(el).remove()
       }
     })
-    const postContentHTML = postContent.html() || ''
 
-    let markdown = turndownService
-      .turndown(postContentHTML)
-      // remove newlines
-      .replace(/\n/g, ' ')
-      // remove multiple spaces
-      .replace(/ +(?= )/g, '')
+    // const postContentHTML = postContent.html() || ''
+    // let markdown = turndownService
+    //   .turndown(postContentHTML)
+    //   // remove newlines
+    //   .replace(/\n/g, ' ')
+    //   // remove multiple spaces
+    //   .replace(/ +(?= )/g, '')
 
-    console.log(postContentStr.length, markdown.length)
+    // console.log(postContentStr.length, markdown.length)
 
-    posts.push(markdown)
-    metadatas.push({ url })
+    // Using text version for now, rather than markdown
+    posts.push(postContentStr)
+    metadatas.push({ url, title })
 
-    console.log(`${url} fetched`)
+    console.log(`${url} fetched, ${postContentStr.length} chars`)
   }
 
   /* Split the text into chunks */
@@ -105,11 +71,14 @@ async function run() {
     separator: '. ',
   })
 
-  const docs = await textSplitter.createDocuments(posts, metadatas)
+  const chunks = await textSplitter.createDocuments(posts, metadatas)
 
   /* Create the vectorstore */
   console.log('Creating vectorstore')
-  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings())
+  const vectorStore = await HNSWLib.fromDocuments(
+    chunks,
+    new OpenAIEmbeddings()
+  )
   vectorStore.save('public/vectorstores/wcpay-docs')
 
   console.log(`Vectorstore created`)
